50c6172e06a098e0dbb354496d2b03aa6690769a
Cash Costello <cash.costello@gmail.com>
2011-11-16 07:54:42 -0500

    removed broken link to non-existent foundation web site

--- globalfooter.php
+++ globalfooter.php
@@ -3,7 +3,7 @@
 	<div id="footer">
 		<div id="footer_right">
 		<span>Elgg is a leading open source social engine.</span>
-		<p>Elgg is an <a href="http://foundation.elgg.org/" target="_blank">Elgg Foundation</a> project.<br />
+		<p>Elgg is an Elgg Foundation project.<br />
 		"Elgg" and the Elgg logo are registered trademarks of Curverider Limited.</p>
 		</div>
 	</div>
