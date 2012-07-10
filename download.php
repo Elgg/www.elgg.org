a440a741020f3e999cb9b761e3620e1857f1324b
Cash Costello <cash.costello@gmail.com>
2012-07-10 06:57:28 -0400

    had the release wrong

--- download.php
+++ download.php
@@ -42,7 +42,7 @@
 		</div>
 		
 		<div id="page_contents">
-			<h1 class="header_color" style="margin-top:40px;">Latest Release - July 9th, 2012</h1>
+			<h1 class="header_color" style="margin-top:40px;">Latest Release - July 10th, 2012</h1>
 			<p class="leader">
 				Elgg 1.8.7 is the latest and recommended version of Elgg.<br />
 			</p>
