ad48b592b6dad439b1d9c440bf8659c2fce0b382
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-09-11 22:58:17 +0300

    chore(release): v1.9.1

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
-<head>		
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Elgg - Open Source Social Networking Engine.</title>
-	<meta name="description" content="Elgg is a leading open source social networking engine which can be used to power your social network." /> 
-	<meta name="keywords" content="open source social networking, social intranet, collaboration software, social networking software, community software, social media, social publishing, social media platform" /> 
+	<meta name="description" content="Elgg is a leading open source social networking engine which can be used to power your social network." />
+	<meta name="keywords" content="open source social networking, social intranet, collaboration software, social networking software, community software, social media, social publishing, social media platform" />
 	<meta name="robots" content="index, follow" />
 	<link rel="shortcut icon" href="favicon.ico" />
 	<link href="style2.css" rel="stylesheet" type="text/css" />
@@ -22,15 +22,15 @@
 				<div id="container_strapline">
 				<div id="tag">
 					<p><span class="strapline accent_color">Introducing a powerful open source<br />social networking engine</span></p>
-					<p class="strap">Providing you with the core components you need<br />to build out socially aware applications</p>	
+					<p class="strap">Providing you with the core components you need<br />to build out socially aware applications</p>
 				</div>
 
 				<div id="download_btn" style="width:250px;">
-					<p><a href="download.php" class="download">Download Elgg 1.9.0</a></p>
+					<p><a href="download.php" class="download">Download Elgg 1.9.1</a></p>
 				</div>
 
 				</div>
-		</div>		
+		</div>
 		<div style="clear:both;"></div>
 		<div id="bottom"><!-- open bottom -->
 		<h2>Elgg is an award-winning social networking engine, delivering the building blocks that enable businesses, schools, universities and associations to create their own fully-featured social networks and applications. Organizations with networks <a href="powering.php">powered by Elgg</a> include:</h2>
@@ -56,20 +56,20 @@
 		<h1 class="header_color">Hosting</h1>
 		<h2>Looking for a provider to host your network built on the open source engine? Find companies who specialise in <a href="hosting.php">Elgg hosting</a>.</h2>
 	</div>
-	
+
 	<div class="twocolbox right">
 		<h1 class="header_color">Giving back</h1>
 		<h2>There are several ways you can help out the Elgg project. The options range from <a href="http://trac.elgg.org">bug reporting</a> and <a href="http://trac.elgg.org">patch submission</a> to joining the <a href="http://elgg.org/supporter.php">supporters scheme</a> or buying a <a href="http://elgg.org/services.php">services listing</a>.</h2>
 	</div>
 <div style="clear:both;"></div>
 </div>
-<div style="clear: both;"></div>	
+<div style="clear: both;"></div>
 </div><!-- // content -->
 
 <?php include("globalfooter.php"); ?>
 </div><!-- // content wrapper -->
 <?php include 'urchin.php'; ?>
-<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script> 
-<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/elgg.json?callback=twitterCallback2&count=1"></script> 
+<script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
+<script type="text/javascript" src="http://twitter.com/statuses/user_timeline/elgg.json?callback=twitterCallback2&count=1"></script>
 </body>
 </html>
