6f4763bdaa9cf1b9958917a53aa416a960b8b9dd
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-09-19 16:38:38 +0300

    Replaces obsolete URLs  pointing to Trac and Google groups

--- developers.php
+++ developers.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
-<head>		
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Elgg - Open Source Social Networking Engine.</title>
-	<meta name="description" content="Elgg is a leading open source social networking engine which can be used to power your social network." /> 
-	<meta name="keywords" content="open source social networking, social intranet, collaboration software, social networking software, community software, social media, social publishing, social media platform" /> 
+	<meta name="description" content="Elgg is a leading open source social networking engine which can be used to power your social network." />
+	<meta name="keywords" content="open source social networking, social intranet, collaboration software, social networking software, community software, social media, social publishing, social media platform" />
 	<meta name="robots" content="index, follow" />
 	<link rel="shortcut icon" href="favicon.ico" />
 	<link href="style.css" rel="stylesheet" type="text/css" />
@@ -38,26 +38,26 @@
 			<h1 class="header_color">Developer Center</h1>
 			<p>Welcome to the elgg.org developer center. If you are interested in developing on the Elgg engine, then this page will point you to all the resources you might need.</p>
 			<p>As always, if you like what you see with Elgg then please consider getting involved in its development.</p>
-			<div class="award"><p>					
+			<div class="award"><p>
 				<a href="http://docs.elgg.org"><img src="images/docs.gif" alt="Documentation" align="left" style="margin-right:10px;" width="60px" /></a>
-				<b>Documentation</b><br />Here you will find information about Elgg's architecture, approach and features.</p> 
-				<div style="clear:both;"></div> 
-			</div>	
-			<div class="award"><p>					
-				<a href="http://trac.elgg.org"><img src="images/bugs.gif" alt="Documentation" align="left" style="margin-right:10px;" width="60px" /></a>
-				<b>Bug Tracker</b><br />This is Elgg's bug tracker, if you find bugs or have patches for existing bugs then head on over and get involved.</p> 
-				<div style="clear:both;"></div> 
+				<b>Documentation</b><br />Here you will find information about Elgg's architecture, approach and features.</p>
+				<div style="clear:both;"></div>
 			</div>
-			<div class="award"><p>					
-				<a href="http://groups.google.com/group/elgg-development/"><img src="images/mailing.gif" alt="Documentation" align="left" style="margin-right:10px;" width="60px" /></a>
-				<b>Developer mailing list</b><br />This mailing list is aimed at experienced developers, not end users.</p> 
-				<div style="clear:both;"></div> 
+			<div class="award"><p>
+				<a href="https://github.com/Elgg/Elgg/blob/master/CONTRIBUTING.md"><img src="images/bugs.gif" alt="Documentation" align="left" style="margin-right:10px;" width="60px" /></a>
+				<b>Bug Tracker</b><br />This is Elgg's bug tracker, if you find bugs or have patches for existing bugs then head on over and get involved.</p>
+				<div style="clear:both;"></div>
 			</div>
-			<div class="award"><p>					
-				<a href="http://reference.elgg.org"><img src="images/api_reference.gif" alt="Documentation" align="left" style="margin-right:10px;" width="60px" /></a>
-				<b>API Reference</b><br />This is a handy resource to search and find out what functions exist within Elgg.</p> 
-				<div style="clear:both;"></div> 
-			</div>	
+			<div class="award"><p>
+				<a href="https://community.elgg.org/groups/profile/211069/feedback-and-planning"><img src="images/mailing.gif" alt="Documentation" align="left" style="margin-right:10px;" width="60px" /></a>
+				<b>Feedback and Planning</b><br />Discussions about the past, present, and future of Elgg.</p>
+				<div style="clear:both;"></div>
+			</div>
+			<div class="award"><p>
+				<a href="http://reference.elgg.org/1.9/"><img src="images/api_reference.gif" alt="Documentation" align="left" style="margin-right:10px;" width="60px" /></a>
+				<b>API Reference</b><br />This is a handy resource to search and find out what functions exist within Elgg.</p>
+				<div style="clear:both;"></div>
+			</div>
 		<div style="clear:both;"></div>
 	</div>
 </div><!-- // content -->
@@ -65,4 +65,4 @@
 </div>
 <?php include 'urchin.php'; ?>
 </body>
-</html>	
+</html>
