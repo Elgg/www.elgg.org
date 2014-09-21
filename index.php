5c5cac17d9c6ad472a97f86ca63d1d9ed36e1ec1
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-09-21 22:14:22 +0300

    chore(release): v1.9.2

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 				</div>
 
 				<div id="download_btn" style="width:250px;">
-					<p><a href="download.php" class="download">Download Elgg 1.9.1</a></p>
+					<p><a href="download.php" class="download">Download Elgg 1.9.2</a></p>
 				</div>
 
 				</div>
