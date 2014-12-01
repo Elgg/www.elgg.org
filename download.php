cf10e65e46e66849444b54231439a56451006d15
Paweł Sroka <srokap@gmail.com>
2014-12-01 04:25:11 +0100

    chore(release): v1.9.6

--- download.php
+++ download.php
@@ -42,8 +42,8 @@
 		</div>
 
 		<div id="page_contents">
-			<h1 class="header_color" style="margin-top:40px;">Latest Release - November 17, 2014</h1>
-			<?php $elgg19 = '1.9.5'; ?>
+			<h1 class="header_color" style="margin-top:40px;">Latest Release - December 1, 2014</h1>
+			<?php $elgg19 = '1.9.6'; ?>
 			<p class="leader">
 				Elgg <?php echo $elgg19; ?> is the latest and recommended version of Elgg.<br />
 				Please report all bugs to <a href="https://github.com/Elgg/Elgg/issues">github</a>.
--- previous.php
+++ previous.php
@@ -37,6 +37,7 @@
 		</div>
 		<div id="page_contents">
 			<h1 class="header_color">Previous releases and security updates</h1>
+			<p><b>1.9.5</b> - released November 17, 2014 <a href="http://elgg.org/getelgg.php?forward=elgg-1.9.5.zip">Download</a></p>
 			<p><b>1.9.4</b> - released October 19, 2014 <a href="http://elgg.org/getelgg.php?forward=elgg-1.9.4.zip">Download</a></p>
 			<p><b>1.9.3</b> - released October 6, 2014 <a href="http://elgg.org/getelgg.php?forward=elgg-1.9.3.zip">Download</a></p>
 			<p><b>1.9.2</b> - released September 21, 2014 <a href="http://elgg.org/getelgg.php?forward=elgg-1.9.2.zip">Download</a></p>
