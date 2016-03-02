ea975580856327f6db697e26b33335eaac6febe5
Steve Clay <steve@mrclay.org>
2016-03-01 21:53:50 -0500

    Tell users about manual registration when closed

---
+++ views/default/community_theme/new_account_msg.php
@@ -0,0 +1,10 @@
+<?php
+
+if (elgg_get_config('allow_registration')) {
+	return;
+}
+
+?>
+<h3>Get an account!</h3>
+<p>Send your desired username to <a href="mailto:info@elgg.org?subject=New account">info@elgg.org</a>
+	and we can create your account. <span style="opacity:.5">Our registration form is temporarily offline.</span></p>
