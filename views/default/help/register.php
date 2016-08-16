b22e7e348f83eb8ec31f7bb221e8df3265bcb640
Steve Clay <steve@mrclay.org>
2016-08-16 14:06:40 -0400

    feature(plugins): load register form via ajax

--- views/default/help/register.php
+++ views/default/help/register.php
@@ -5,18 +5,19 @@
  */
 $title = elgg_echo("community_theme:title:register");
 
-$body = <<<BODY
+ob_start();
+?>
 <p>To keep this community site useful and safe for all, please:</p>
 <ul>
 	<li>Do not post test content. Test content on the community site will be deleted.</li>
 	<li>Be courteous, friendly, and patient with all members of the site.</li>
-	<li>Be respectful of plugin developers\' time and priorities.</li>
-	<li>Read and follow the <a href="https://elgg.org/terms">Terms and Conditions.</a></li>
+	<li>Be respectful of plugin developers' time and priorities.</li>
+	<li>Read and follow the <a href="<?= elgg_normalize_url('terms') ?>">Terms and Conditions.</a></li>
 </ul>
-BODY;
+<?php
+$body = ob_get_clean();
 
 ?>
-
 <div class="elgg-output">
 	<?php echo elgg_view_module('register', $title, $body); ?>
 </div>
