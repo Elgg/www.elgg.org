9c47ad231d201c4b6e4880d57bc9a0deea0a767d
Steve Clay <steve@mrclay.org>
2016-10-02 15:50:49 -0400

    chore(site): update releases and Elgg to 2.2.2

--- src/Elgg/Releases.php
+++ src/Elgg/Releases.php
@@ -20,6 +20,7 @@ class Releases {
 		'1.10',
 	];
 	static $releases = [
+		'2.2.2' => 'October 2, 2016',
 		'2.2.1' => 'September 21, 2016',
 		'2.2.0' => 'August 5, 2016',
 		'2.2.0-rc.1' => 'June 16, 2016',
@@ -40,6 +41,7 @@ class Releases {
 		'2.0.0-alpha.3' => 'August 24, 2015',
 		'2.0.0-alpha.2' => 'August 6, 2015',
 		'2.0.0-alpha.1' => 'July 10, 2015',
+		'1.12.13' => 'October 2, 2016',
 		'1.12.12' => 'August 5, 2016',
 		'1.12.11' => 'June 13, 2016',
 		'1.12.10' => 'May 29, 2016',
